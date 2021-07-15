6db1ecb4f1f1e7380b7ed6acaee8ba2c070230e7
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2021-07-14 22:13:33 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const app = {};
 app.setUpMap = () => {
     L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
-    const map = L.mapquest.map('map', {
+    app.map = L.mapquest.map('map', {
         center: [43.6532, -79.3832],
         layers: L.mapquest.tileLayer('dark'),
         zoom: 12
     });
-    app.getLocationExample(map);
-    map.addControl(L.mapquest.control());
+    app.markerLayer = L.layerGroup().addTo(app.map);
+    app.map.addControl(L.mapquest.control());
 }
-app.getLocationExample = (map) => {
-    L.mapquest.textMarker([43.74318039, -79.21887334999998], {
-        text: 'Shootings and Firearm Discharges',
-        subtext: 'Scarborough Village (139)',
+app.addLocation = (y, x, textArg, subtextArg) => {
+    L.mapquest.textMarker([y, x], {
+        text: textArg,
+        subtext: subtextArg,
         position: 'right',
         type: 'marker',
         icon: {
@@ -20,11 +20,48 @@ app.getLocationExample = (map) => {
             secondaryColor: '#333333',
             size: 'sm'
         }
-    }).addTo(map);
+    }).addTo(app.markerLayer);
 }
+app.crimefetch = () => {
+  fetch("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Shootings_and_Firearm_Discharges/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json").then(response => response.json()).then(function(data) { 
+  //console.log(data.features[1]); 
+  app.insertLocation(data);});
+}
+
+app.insertLocation = (data) =>{
+	app.crimeData = data;
+  //console.log(app.crimeData);
+  let i = 0;
+  let j = 0;
+  /* app.markerLayer.remove().then(()=>{
+    app.markerLayer = L.layerGroup().addTo(app.map);
+  }); */
+  while (i < app.crimeData.features.length) {
+  	if(app.crimeData.features[i].attributes.Occurrence_year > 2010){
+    	let x = app.crimeData.features[i].geometry.x;
+    	let y = app.crimeData.features[i].geometry.y;
+    	let textArg = app.crimeData.features[i].attributes.Neighbourhood;
+    	let subtextArg = app.crimeData.features[i].attributes.Neighbourhood;
+  		app.addLocation(y, x,textArg, subtextArg);
+      j++;
+    }
+    
+  	i++;
+  }
+  console.log(j);
+}
+
 app.init = () => {
+		
     window.onload = () => {
-        app.setUpMap();
+    			app.setUpMap();
+    			document.querySelector("form").addEventListener("submit",function(event){
+          	event.preventDefault();
+            app.crimefetch();
+            });
+          
     }
 }
 app.init();
+
+
