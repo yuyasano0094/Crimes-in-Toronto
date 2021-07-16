6a79afd32deefcb404da4a9b47d3dba233ed4972
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2021-07-16 17:06:47 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
 const app = {};
+var startyear = 2021;
+var endyear;
+function minYear(x) {
+	if(x < startyear) {
+  startyear = x;
+  }
+}
+
+function chooseYear(year) {
+while(year <= 2020) {
+    var opt = document.createElement('option');
+    opt.value = 2020  - (year - startyear);
+    opt.innerHTML = 2020  - (year - startyear);
+    document.getElementById("yearstart").appendChild(opt);
+    console.log("i did it")
+    year++;
+}
+}
+
+function chooseEndYear(year){
+	document.getElementById("yearEnd").innerHTML=""
+  let yearStarter = year.target.value;
+  while(yearStarter<=2021) {
+    var opt = document.createElement('option');
+    opt.value = 2021  - (yearStarter - year.target.value);
+    opt.innerHTML = 2021  - (yearStarter - year.target.value);
+    document.getElementById("yearEnd").appendChild(opt);
+    console.log("i did it")
+    yearStarter++;
+  }
+}
+
+fetch("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Shootings_and_Firearm_Discharges/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json").then(response => response.json()).then(function(data) {
+for(var i = 0; i < data.features.length; i++) {
+if(data.features[i].attributes.Occurrence_year < startyear) {
+startyear = data.features[i].attributes.Occurrence_year;
+}
+}
+
+return startyear;
+}).then(chooseYear);
+
+document.getElementById("yearstart").onchange = chooseEndYear;
+
 app.setUpMap = () => {
     L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
     app.map = L.mapquest.map('map', {
@@ -6,7 +50,8 @@ app.setUpMap = () => {
         layers: L.mapquest.tileLayer('dark'),
         zoom: 12
     });
-    app.markerLayer = L.layerGroup().addTo(app.map);
+   	app.markerLayer = L.layerGroup().addTo(app.map);
+    app.arrayCounter++;
     app.map.addControl(L.mapquest.control());
 }
 app.addLocation = (y, x, textArg, subtextArg) => {
@@ -24,7 +69,8 @@ app.addLocation = (y, x, textArg, subtextArg) => {
 }
 app.crimefetch = () => {
   fetch("https://services.arcgis.com/S9th0jAJ7bqgIRjw/arcgis/rest/services/Shootings_and_Firearm_Discharges/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json").then(response => response.json()).then(function(data) { 
-  //console.log(data.features[1]); 
+  //console.log(data.features[1]);
+  console.log(data);
   app.insertLocation(data);});
 }
 
@@ -37,7 +83,7 @@ app.insertLocation = (data) =>{
     app.markerLayer = L.layerGroup().addTo(app.map);
   }); */
   while (i < app.crimeData.features.length) {
-  	if(app.crimeData.features[i].attributes.Occurrence_year > 2010){
+  	if(app.crimeData.features[i].attributes.Occurrence_year >= document.getElementById("yearstart").value && app.crimeData.features[i].attributes.Occurrence_year <= document.getElementById("yearEnd").value){
     	let x = app.crimeData.features[i].geometry.x;
     	let y = app.crimeData.features[i].geometry.y;
     	let textArg = app.crimeData.features[i].attributes.Neighbourhood;
@@ -54,10 +100,11 @@ app.insertLocation = (data) =>{
 app.init = () => {
 		
     window.onload = () => {
-    			app.setUpMap();
-    			document.querySelector("form").addEventListener("submit",function(event){
+     				app.setUpMap();
+           document.querySelector("form").addEventListener("submit",function(event){
           	event.preventDefault();
-            app.crimefetch();
+            	app.markerLayer.clearLayers();
+              app.crimefetch();
             });
           
     }
